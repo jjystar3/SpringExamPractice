d9f7d83951a3fbe9be4310b6f6fe5f45cb185627
JuYoung <jjystar3@gmail.com>
2024-09-27 15:03:33 +0900

    first commit

---
+++ src/main/java/com/example/demo/SpringExamPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamPracticeApplication.class, args);
+	}
+
+}
